3421487f6c796a7c14939232cb280a33713c8ca2
Mouse <mouse0w0@qq.com>
2020-03-18 12:08:44 +0800

    feat: add @FunctionalInterface

--- src/main/java/engine/command/simple/CommandArgumentChecker.java
+++ src/main/java/engine/command/simple/CommandArgumentChecker.java
@@ -3,6 +3,7 @@ package engine.command.simple;
 import engine.command.ArgumentCheckResult;
 import engine.command.CommandSender;
 
+@FunctionalInterface
 public interface CommandArgumentChecker {
     ArgumentCheckResult checkArguments(CommandSender sender, String[] args);
 }
--- src/main/java/engine/command/simple/CommandTips.java
+++ src/main/java/engine/command/simple/CommandTips.java
@@ -4,6 +4,7 @@ import engine.command.CommandSender;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface CommandTips {
     List<String> getTips(CommandSender sender, String[] args);
 }
