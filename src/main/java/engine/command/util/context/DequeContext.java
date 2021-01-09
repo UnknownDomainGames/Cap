805aeb3fb2bdcd89ebb4936bb088f8d62a040853
defoli <defoli_ation@outlook.com>
2021-01-09 12:09:40 +0800

    style: rename

---
+++ src/main/java/engine/command/util/context/DequeContext.java
@@ -0,0 +1,5 @@
+package engine.command.util.context;
+
+import java.util.Deque;
+
+public interface DequeContext extends Context, Deque<Object> {}
--- src/main/java/engine/command/util/context/DequeLinkedContext.java
+++
@@ -1,5 +0,0 @@
-package engine.command.util.context;
-
-import java.util.Deque;
-
-public interface DequeLinkedContext extends Context, Deque<Object> {}
